8e16725f3fa13412d67117bde70d12810487067f
alexandrgv <algvanceladze@gmail.com>
2023-06-20 13:25:38 +0400

    Update simpleGitHub.go

--- simpleGitHub/simpleGitHub.go
+++ simpleGitHub/simpleGitHub.go
@@ -1,19 +1,5 @@
-/*
 package simpleGitHub
 
 func AddTwo(x, y int) int {
   return x + y
-}
-*/
-
-package main
-
-import (
-	"fmt"
-
-	"golang.org/x/example/stringutil"
-)
-
-func main() {
-	fmt.Println(stringutil.Reverse("!selpmaxe oG ,olleH"))
 }
